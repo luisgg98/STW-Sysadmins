7331520abc1544c697eeaeab2d9dbb6f021f9833
Luis Garcia <luisgarcia98@hotmail.es>
2021-03-15 16:26:06 +0100

    Structuring backend I've structured the project according to the classes there are several files and code which are usless and need to be change

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -13,7 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// Using morgan, module for logging
+app.use(morgan('dev'));
+//Format indicates how we want to see the loogging
+
+// Allow nodejs to use JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
