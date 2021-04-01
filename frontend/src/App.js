273df70294d7ffdc87469dd8f8618321a1036da6
Alberto Calvo <albertocalvojaca@gmail.com>
2021-04-01 18:04:49 +0200

    simple login mock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,46 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { useState } from "react";
+import LoginForm from "./components/LoginForm"
 
 export default function App() {
+
+  const userDB = {
+    email: "user@user.com",
+    password: "user123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+
+    if (details.email === userDB.email && details.password === userDB.password) {
+      console.log("Logged in")
+      setUser({
+        name: details.name,
+        email: details.email
+      })
+    } else {
+      console.log("Details do not match!")
+      setError("Details do not match!")
+    }
+  }
+
+  const Logout = () => {
+    console.log("Logout")
+    setUser({name: "", email: ""})
+  }
+
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    <div className="App">
+      {(user.email !== "") ? (
+            <div className="welcome">
+              <h1>Welcome, <span>{user.name}</span></h1>
+              <button onClick={Logout}>LogOut</button>
+            </div>
+          ) : (
+        <LoginForm Login={Login} error={error}/>)}
+    </div>
+    )  
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
