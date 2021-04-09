30928840fb8689a3dec07faf1f7ed730d5ed06d4
hayk99 <haykk1999@gmail.com>
2021-04-09 11:18:56 +0200

    login form async. validation done. missing login error feedback

--- frontend/src/components/common/Forms/LoginForm.js
+++ frontend/src/components/common/Forms/LoginForm.js
@@ -5,14 +5,18 @@ import { Button, Form } from "react-bootstrap";
 import { UserContext } from "../../../UserContext"
 import API from "../../../services/AuthService"
 import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { Alert } from "react-bootstrap";
+
 
 const LoginForm = () => {
-
     // Datos del usuario hacer login
     const { user, setUser } = useContext(UserContext);
 
+    const [validated, setValidated] = useState(false);
+
     // Datos del formulario
-    const [details, setDetails] = useState({
+    const [formValue, setForm] = useState({
         email: "",
         password: "",
     });
@@ -21,49 +25,78 @@ const LoginForm = () => {
     const [error, setError] = useState("");
 
 
-    function handleSubmit(e) {
-        e.preventDefault();
+
+
+    const { formState: { errors, touchedFields }, register, handleSubmit } = useForm();
+
+    const onSubmit = async (e, data) => {
         console.log("handling submit");
 
         require('axios-debug-log')
-        axios.post(`https://stw-zitation.herokuapp.com/api/users/login`, details,
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then(
-                (response) => {
-                    if (response.status === 200) {
-                        setUser({
-                            email: details.email,
-                        })
+        try {
+            const response = await axios.post(`https://stw-zitation.herokuapp.com/api/users/login`, formValue,
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
                     }
-                    else {
-                        console.log("error 40x");
-                    }
-                    console.log(response.status);
-                }
-            ).catch(e => {
-                console.log('catch error');
-                setUser({
-                    email: ''
                 })
+            if (response.status === 200) {
+                setUser({
+                    email: formValue.email,
+                })
+            }
+            else {
+                console.log("error 40x");
+            }
+            console.log(response.status);
+        }
+        catch (e) {
+            console.log('catch error');
+            setError({ type: "wrong login" })
+            setForm({
+                email: '',
+                password: ''
             })
+            setUser({
+                email: ''
+            })
+
+        }
     }
+
+
 
     return (
         <div>
-            <Form noValidate onSubmit={handleSubmit} >
+            <Form noValidate onSubmit={handleSubmit(onSubmit)} >
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control
                         type="email"
+                        name="email"
                         placeholder="Enter email"
-                        value={details.email}
-                        onChange={(e) =>
-                            setDetails({ ...details, email: e.target.value })
-                        } 
-                        onError='error'/>
+                        value={formValue.email}
+                        {...register("email", {
+                            required: { value: true, message: "Password is required" },
+                            pattern: {
+                                value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                                message: "Wrong email format"
+                            }
+                        })}
+                        onChange={(e) => {
+                            setForm({ ...formValue, email: e.target.value })
+                            if (errors.email) {
+                                // console.log("hay error");
+                                // console.log(errors.password.message)
+                                setError(errors.email.message)
+                            }
+                        }
+                        }
+                        isInvalid={errors.email && touchedFields.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Invalid email
+                    </Form.Control.Feedback>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                             </Form.Text>
@@ -74,11 +107,23 @@ const LoginForm = () => {
                     <Form.Control
                         type="password"
                         placeholder="Password"
-                        value={details.password}
-                        onChange={(e) =>
-                            setDetails({ ...details, password: e.target.value })
+                        value={formValue.password}
+                        {...register("password", {
+                            required: true,
+                            minLength: { value: 8, message: "minimum legth is 8" },
+                            // pattern: { value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/, message: "Password must contain uppercase, lowercase, simbol and numbers." }
+                        })}
+                        onChange={(e) => {
+                            setForm({ ...formValue, password: e.target.value })
+                            if (errors.password) {
+                                // console.log("hay error");
+                                // console.log(errors.password.message)
+                                setError(errors.password.message)
+                            }
                         }
-                        onError='error' />
+                        }
+                        isInvalid={errors.password && touchedFields.password} />
+                    <Form.Control.Feedback type="invalid">Min legth is 8. Must contain uppercase, symbol and numbers.</Form.Control.Feedback>
                 </Form.Group>
                 <Button variant="primary" type="submit">Log In</Button>
             </Form>
