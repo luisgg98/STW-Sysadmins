9159ba62941bf108e51d64d715868d6f6e1e7e82
German Garces <757024@unizar.es>
2021-04-22 14:59:34 +0200

    Added CRUD services

--- test/models/services.js
+++ test/models/services.js
@@ -3,7 +3,7 @@ const Service = require('../../models/service')
 require('../../config/database')
 const newdesc = 'Rodolfo Mascarpone'
 
-const ServicesArray = [
+const servicesArray = [
     {company: "73448121J",description:"adsf",capacity:2,price:10},
     {company: "73448121J",description:"adasdfsf",capacity:2,price:10},
     {company: "73448121J",description:"adasdfsf",capacity:2,price:10}
@@ -28,8 +28,8 @@ describe('Service model tests',function () {
     })
     after(function (done) {
         let numServiceRemoved = 0
-        ServicesArray.forEach(function (ServiceData) {
-            Service.findOneAndRemove({description: ServiceData.description},{},function (err, doc) {
+        servicesArray.forEach(function (serviceData) {
+            Service.findOneAndRemove({description: serviceData.description},{},function (err, doc) {
                 if (err) throw err;
                 numServiceRemoved++;
                 if (numServiceRemoved == (ServicesArray.length-1)){
@@ -40,14 +40,14 @@ describe('Service model tests',function () {
     })
 
     it('Should find all Services without error',function (done) {
-        Service.find(function (error, Services) {
-            assert.isAtLeast(Services.length,3)
+        Service.find(function (error, services) {
+            assert.isAtLeast(services.length,3)
             done();
         })
     })
 
     it('Should find a Service by name',function (done) {
-        Service.findOne({description:ServicesArray[1].description}, function (err,doc) {
+        Service.findOne({description:servicesArray[1].description}, function (err,doc) {
             if (err) throw  err;
             assert.isNotNull(doc)
             done();
@@ -55,19 +55,19 @@ describe('Service model tests',function () {
     })
 
     it('Should change the description of a Service',function (done) {
-        Service.findOneAndUpdate({description:ServicesArray[0].description},{description:newdesc},
+        Service.findOneAndUpdate({description:servicesArray[0].description},{description:newdesc},
             {returnOriginal:false, upsert:true, new:true},
             function (err,doc) {
                 if(err){ throw err;}
                 assert.isNotNull(doc);
                 assert.equal(doc.description,newdesc);
-                ServicesArray[0].description = newdesc;
+                servicesArray[0].description = newdesc;
                 done();
             })
     })
 
     it('Should delete a Service by name',function (done) {
-        Service.findOneAndRemove({description: ServicesArray[0].description},{},function (err, doc) {
+        Service.findOneAndRemove({description: servicesArray[0].description},{},function (err, doc) {
             if (err) throw err;
             assert.isNotNull(doc)
             done();
