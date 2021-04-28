72e023417a37abfb836585c9c8c82266b59d6e0b
Luis Garcia <luisgarcia98@hotmail.es>
2021-04-28 11:01:25 +0200

    Test configuration

--- frontend/src/test/config/conf.js
+++ frontend/src/test/config/conf.js
@@ -1,8 +1,30 @@
 exports.config = {
+    framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     baseUrl: 'http://localhost:3001',
+    //specs: ['../e2e/registerCompany.js'],
     specs: ['../e2e/*.js'],
-    onPrepare: () => {
-        browser.ignoreSynchronization = true
+    multiCapabilities: [{
+        browserName: 'firefox'
+    }, {
+        browserName: 'chrome'
+    }],
+    // Options to be passed to Jasmine.
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    },
+    onPrepare: async () => {
+        await browser.waitForAngularEnabled(false);
+        browser.ignoreSynchronization = true;
+        var Faker = require('faker');
+        browser.params.randomName = Faker.name.findName(); // Rowan Nikolaus
+        browser.params.randomEmail = Faker.internet.email();
+        browser.params.randomPhone = Faker.phone.phoneNumber();
+        browser.params.companyName= 'Centro Deportivo Municipal La Granja';
+        browser.params.streetNumber=45;
+        browser.params.street='Camino Cabaldós';
+        browser.params.zipcode=50013;
+        browser.params.nif = 123456789;
+        browser.params.password = Faker.internet.password();
     },
 };
--- frontend/src/test/e2e/homePage.js
+++ frontend/src/test/e2e/homePage.js
@@ -1,12 +1,19 @@
 
 //https://www.protractortest.org/#/tutorial
 describe('Home page testing', function() {
+
     it('Testing Search Bar', function() {
+        var EC = protractor.ExpectedConditions;
         browser.get('');
         //browser.wait(500);
         element(by.id('barraBusqueda')).click();
         element(by.id('barraBusqueda')).sendKeys('Deportivo');
+        //browser.actions().sendKeys(protractor.Key.ENTER).perform();
         element(by.id('barraBusqueda')).sendKeys(protractor.Key.ENTER);
+        var e = element(by.className('card-title h5'));
+        browser.wait(EC.presenceOf(e), 10000);
+        expect(e.isPresent()).toBeTruthy();
+        browser.sleep(6000);
 
     });
 });
