aad2b12a8c2719505d30e4d9291990370d3f5091
Luis Garcia <luisgarcia98@hotmail.es>
2021-05-12 13:34:04 +0200

    Deleting useless functions

--- services/update_time_slots.js
+++ services/update_time_slots.js
@@ -57,213 +57,67 @@ function update_time_slots(company) {
     console.log("UPDATING TIME SLOTS")
     let times = {};
     let day_times;
-    if (company.schedule.monday.open_1 && company.schedule.monday.close_1) {
-        day_times = calculateTime(company.schedule.monday.open_1, company.schedule.monday.close_1, company.service_duration);
-        times.monday_1 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.tuesday.open_1 && company.schedule.tuesday.close_1) {
-        day_times = calculateTime(company.schedule.tuesday.open_1, company.schedule.tuesday.close_1, company.service_duration);
-        times.tuesday_1 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.wednesday.open_1 && company.schedule.wednesday.close_1) {
-        day_times = calculateTime(company.schedule.wednesday.open_1, company.schedule.wednesday.close_1, company.service_duration);
-        times.wednesday_1 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.thursday.open_1 && company.schedule.thursday.close_1) {
-        day_times = calculateTime(company.schedule.thursday.open_1, company.schedule.thursday.close_1, company.service_duration);
-        times.thursday_1 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.friday.open_1 && company.schedule.friday.close_1) {
+    if (company.service_duration > 0) {
+        if (company.schedule.monday.open_1 && company.schedule.monday.close_1) {
+            day_times = calculateTime(company.schedule.monday.open_1, company.schedule.monday.close_1, company.service_duration);
+            times.monday_1 = day_times
+        }
+        if (company.schedule.tuesday.open_1 && company.schedule.tuesday.close_1) {
+            day_times = calculateTime(company.schedule.tuesday.open_1, company.schedule.tuesday.close_1, company.service_duration);
+            times.tuesday_1 = day_times
+        }
+        if (company.schedule.wednesday.open_1 && company.schedule.wednesday.close_1) {
+            day_times = calculateTime(company.schedule.wednesday.open_1, company.schedule.wednesday.close_1, company.service_duration);
+            times.wednesday_1 = day_times
+        }
+        if (company.schedule.thursday.open_1 && company.schedule.thursday.close_1) {
+            day_times = calculateTime(company.schedule.thursday.open_1, company.schedule.thursday.close_1, company.service_duration);
+            times.thursday_1 = day_times
+        }
+        if (company.schedule.friday.open_1 && company.schedule.friday.close_1) {
 
-        day_times = calculateTime(company.schedule.friday.open_1, company.schedule.friday.close_1, company.service_duration);
-        times.friday_2 = day_times
-        //times = times.concat(day_times);
+            day_times = calculateTime(company.schedule.friday.open_1, company.schedule.friday.close_1, company.service_duration);
+            times.friday_2 = day_times
+        }
+        if (company.schedule.saturday.open_1 && company.schedule.saturday.close_1) {
+            day_times = calculateTime(company.schedule.saturday.open_1, company.schedule.saturday.close_1, company.service_duration);
+            times.saturday_2 = day_times
+        }
+        if (company.schedule.sunday.open_1 && company.schedule.sunday.close_1) {
+            day_times = calculateTime(company.schedule.sunday.open_1, company.schedule.sunday.close_1, company.service_duration);
+            times.sunday_2 = day_times
+        }
+        if (company.schedule.monday.open_2 && company.schedule.monday.close_2) {
+            day_times = calculateTime(company.schedule.monday.open_2, company.schedule.monday.close_2, company.service_duration);
+            times.monday_2 = day_times
+        }
+        if (company.schedule.tuesday.open_2 && company.schedule.tuesday.close_2) {
+            day_times = calculateTime(company.schedule.tuesday.open_2, company.schedule.tuesday.close_2, company.service_duration);
+            times.tuesday_2 = day_times
+        }
+        if (company.schedule.wednesday.open_2 && company.schedule.wednesday.close_2) {
+            day_times = calculateTime(company.schedule.wednesday.open_2, company.schedule.wednesday.close_2, company.service_duration);
+            times.wednesday_2 = day_times
+        }
+        if (company.schedule.thursday.open_2 && company.schedule.thursday.close_2) {
+            day_times = calculateTime(company.schedule.thursday.open_2, company.schedule.thursday.close_2, company.service_duration);
+            times.thursday_2 = day_times
+        }
+        if (company.schedule.friday.open_2 && company.schedule.friday.close_2) {
+            day_times = calculateTime(company.schedule.friday.open_2, company.schedule.friday.close_2, company.service_duration);
+            times.friday_2 = day_times
+        }
+        if (company.schedule.saturday.open_2 && company.schedule.saturday.close_2) {
+            day_times = calculateTime(company.schedule.saturday.open_2, company.schedule.saturday.close_2, company.service_duration);
+            times.saturday_2 = day_times
+        }
+        if (company.schedule.sunday.open_2 && company.schedule.sunday.close_2) {
+            day_times = calculateTime(company.schedule.sunday.open_2, company.schedule.sunday.close_2, company.service_duration);
+            times.sunday_2 = day_times
+        }
     }
-    if (company.schedule.saturday.open_1 && company.schedule.saturday.close_1) {
-        day_times = calculateTime(company.schedule.saturday.open_1, company.schedule.saturday.close_1, company.service_duration);
-        times.saturday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.sunday.open_1 && company.schedule.sunday.close_1) {
-        day_times = calculateTime(company.schedule.sunday.open_1, company.schedule.sunday.close_1, company.service_duration);
-        times.sunday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.monday.open_2 && company.schedule.monday.close_2) {
-        day_times = calculateTime(company.schedule.monday.open_2, company.schedule.monday.close_2, company.service_duration);
-        times.monday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.tuesday.open_2 && company.schedule.tuesday.close_2) {
-        day_times = calculateTime(company.schedule.tuesday.open_2, company.schedule.tuesday.close_2, company.service_duration);
-        times.tuesday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.wednesday.open_2 && company.schedule.wednesday.close_2) {
-        day_times = calculateTime(company.schedule.wednesday.open_2, company.schedule.wednesday.close_2, company.service_duration);
-        times.wednesday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.thursday.open_2 && company.schedule.thursday.close_2) {
-        day_times = calculateTime(company.schedule.thursday.open_2, company.schedule.thursday.close_2, company.service_duration);
-        times.thursday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.friday.open_2 && company.schedule.friday.close_2) {
-        day_times = calculateTime(company.schedule.friday.open_2, company.schedule.friday.close_2, company.service_duration);
-        times.friday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.saturday.open_2 && company.schedule.saturday.close_2) {
-        day_times = calculateTime(company.schedule.saturday.open_2, company.schedule.saturday.close_2, company.service_duration);
-        times.saturday_2 = day_times
-        //times = times.concat(day_times);
-    }
-    if (company.schedule.sunday.open_2 && company.schedule.sunday.close_2) {
-        day_times = calculateTime(company.schedule.sunday.open_2, company.schedule.sunday.close_2, company.service_duration);
-        times.sunday_2 = day_times
-        //times = times.concat(day_times);
-    }
+
     return times;
 }
 
-// SERVICES
-
-function update_time_slots_services(company, capacity) {
-    let time_slots = company.time_slots
-    console.log(time_slots)
-    let time_slots_services = {}
-    if (time_slots.monday_1) {
-        time_slots_services.monday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.monday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.monday_1.places_left = times
-        time_slots_services.monday_1.slots = time_slots.monday_1
-    }
-    if (time_slots.monday_2) {
-        time_slots_services.monday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.monday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.monday_2.places_left = times
-        time_slots_services.monday_2.slots = time_slots.monday_2
-    }
-    if (time_slots.tuesday_1) {
-        time_slots_services.tuesday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.tuesday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.tuesday_1.places_left = times
-        time_slots_services.tuesday_1.slots = time_slots.tuesday_1
-    }
-    if (time_slots.tuesday_2) {
-        time_slots_services.tuesday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.tuesday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.tuesday_2.places_left = times
-        time_slots_services.tuesday_2.slots = time_slots.tuesday_2
-    }
-    if (time_slots.wednesday_1) {
-        time_slots_services.wednesday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.wednesday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.wednesday_1.places_left = times
-        time_slots_services.wednesday_1.slots = time_slots.wednesday_1
-    }
-    if (time_slots.wednesday_2) {
-        time_slots_services.wednesday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.wednesday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.wednesday_2.places_left = times
-        time_slots_services.wednesday_2.slots = time_slots.wednesday_2
-    }
-    if (time_slots.thursday_1) {
-        time_slots_services.thursday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.thursday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.thursday_1.places_left = times
-        time_slots_services.thursday_1.slots = time_slots.thursday_1
-    }
-    if (time_slots.thursday_2) {
-        time_slots_services.thursday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.thursday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.thursday_2.places_left = times
-        time_slots_services.thursday_2.slots = time_slots.thursday_2
-    }
-    if (time_slots.friday_1) {
-        time_slots_services.friday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.friday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.friday_1.places_left = times
-        time_slots_services.friday_1.slots = time_slots.friday_1
-    }
-    if (time_slots.friday_2) {
-        time_slots_services.friday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.friday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.friday_2.places_left = times
-        time_slots_services.friday_2.slots = time_slots.friday_2
-    }
-    if (time_slots.saturday_1) {
-        time_slots_services.saturday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.saturday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.saturday_1.places_left = times
-        time_slots_services.saturday_1.slots = time_slots.saturday_1
-    }
-    if (time_slots.saturday_2) {
-        time_slots_services.saturday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.saturday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.saturday_2.places_left = times
-        time_slots_services.saturday_2.slots = time_slots.saturday_2
-    }
-    if (time_slots.sunday_1) {
-        time_slots_services.sunday_1 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.sunday_1.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.sunday_1.places_left = times
-        time_slots_services.sunday_1.slots = time_slots.sunday_1
-    }
-    if (time_slots.sunday_2) {
-        time_slots_services.sunday_2 = {}
-        let times = []
-        for (let i = 0; i < company.time_slots.sunday_2.length; i++) {
-            times[i] = capacity
-        }
-        time_slots_services.sunday_2.places_left = times
-        time_slots_services.sunday_2.slots = time_slots.sunday_2
-    }
-    return time_slots_services
-}
-
-module.exports = {update_time_slots, update_time_slots_services};
+module.exports = {update_time_slots};
